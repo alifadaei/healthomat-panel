a79dcd31e6124864f3b013fe6d9000a9d100f965
Ali <alifadaeimanesh@gmail.com>
2022-09-15 15:09:47 +0430

    preload locales

--- src/i18next.ts
+++ src/i18next.ts
@@ -1,12 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+// import Backend from "i18next-http-backend";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
-  .use(Backend)
+  // .use(Backend)
   .init({
     debug: true,
     lng: "en",
